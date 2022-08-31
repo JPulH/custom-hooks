f19e47eaf6557eefb8fbc6e355ff69e365dbd34e
JPulH <jpulh.0310@gmail.com>
2022-08-31 11:55:45 -0500

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export const useForm = (initialForm = {}) => {
+  
+    const[formState, setformState] = useState(initialForm);
+
+    const onInputChange = ({target}) => {
+        // console.log(event.target)
+        const {name,value} = target;
+        // console.log({target})
+        setformState({
+            ...formState,
+            [name]: value
+        });
+    }
+
+    const onResetForm = () => {
+        setformState(initialForm);
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm
+
+    }
+}
